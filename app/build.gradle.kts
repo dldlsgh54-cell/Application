726d9f327817c23fbf546b30ae153b19b72564dd
이인호(INHO LEE)[lihdldlsgh] <lihdldlsgh@LGENSOL.GLOBAL>
2026-06-30 10:40:43 +0900

    Fix Compose BOM version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.09.0"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
